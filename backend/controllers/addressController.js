35c49c93fe78c3587aee67945873b46bf49ba716
priyanshuaagrawal <168630260+priyanshuaagrawal@users.noreply.github.com>
2025-08-04 18:24:04 +0530

    Add files via upload

---
+++ backend/controllers/addressController.js
@@ -0,0 +1,27 @@
+import Address from "../models/Address.js"
+
+// add Address: /api/address/add
+
+export const addAddress = async(req, res)=>{
+    try{
+        const {address} = req.body
+        const userId = req.userId
+        await Address.create({...address, userId})
+        res.json({success: true, message: "Address added Successfully"})
+    } catch(error){
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
+
+//Get Address: /api/address/get
+export const getAddress = async(req, res)=>{
+    try{
+        const userId = req.userId
+        const addresses = await Address.find({userId})
+        res.json({success: true, addresses})
+    } catch(error){
+        console.log(error.message)
+        res.json({success: false, message: error.message})
+    }
+}
